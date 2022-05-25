38abbb804937655e8a856d4c00e715bc0f72827f
oharakogakuin <ohara@g.neec.ac.jp>
2022-05-25 11:06:32 +0900

    02_02.ビュー変換行列

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -59,7 +59,10 @@ class GameScene {
 	Model* model_ = nullptr;
 
 	// ワールドトランスフォーム
-	WorldTransform worldTransform_;
+	WorldTransform worldTransform_[100];
 	// ビュープロジェクション
 	ViewProjection viewProjection_;
+
+	// カメラ上方向の角度
+	float viewAngle = 0.0f;
 };
